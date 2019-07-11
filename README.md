2faaa316028d47fb5f4c72f038099b57c3ebd8b9
Andres-Delgado <andres36@mail.usf.edu>
2019-07-11 18:50:25 -0400

    Made function to extract the output statements inside a Java function

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ functions: {
 	funcName1: {
 		"parameters": [name1, name2, ...],
 		"localVars": [name1, name2, ...],
+		"content": [statement1, statement2, ...]
 		...
 	},
 	funcName2: {
--- automate.py
+++ automate.py
@@ -82,6 +82,9 @@ def ExtractFunction(functionStr):
 
 			# extract the parameter names, add to dictionary
 			functionDict[nameStr]["parameters"] = ExtractParameterVars(tempList)
+
+			# create a key for the contents in the function
+			functionDict[nameStr]["content"] = []
 			break
 
 		# skip function and parameter prefixes
@@ -90,9 +93,63 @@ def ExtractFunction(functionStr):
 
 		tempList.pop(0)
 
-	print("Current Function Dictionary:")
-	print(json.dumps(functionDict, indent = 2), end = '\n\n')
+	#print("Current Function Dictionary:")
+	#print(json.dumps(functionDict, indent = 2), end = '\n\n')
+	
 	return functionDict
+
+def ExtractContents(javaContent):
+	"""Converts the statements inside a Java function
+	into Python statements. The new Python statements
+	are chronologically ordered inside contentList."""
+
+	contentList = []
+	tempList = list(javaContent)
+
+	# iterate until ending '}' is found
+	while tempList[0] != "}":
+
+		# found output statement
+		if "System.out.print" in tempList[0]:
+			
+			newline = "println" in tempList[0]
+			opString = tempList[0][tempList[0].index("\""):] + " "
+			tempList.pop(0)
+
+			# iterate through print statement until ending quotation char
+			while "\"" not in tempList[0]:
+				opString += tempList[0] + " "
+				tempList.pop(0)
+
+			# output is just a string
+			if "\");" in tempList[0]:
+				opString += tempList[0][:tempList[0].index("\");") + 1]
+				
+			# output contains more just a string
+			else:
+
+				# iterate until ';' char
+				while ";" not in tempList[0]:
+					opString += tempList[0] + " "
+					tempList.pop(0)
+
+				opString += tempList[0][:tempList[0].index(");")]
+				
+			# output does not contain a new line
+			if not newline:
+				opString += ", end = \"\""
+
+			opString = "print(" + opString + ")"
+			print("print statement:", opString)
+			contentList.append(opString)
+
+		#else:
+		#	print(tempList[0])
+
+		tempList.pop(0)
+
+	print()
+	return contentList
 
 def Parse(javaList):
 	"""Parses through the list of strings in javaList
@@ -103,7 +160,7 @@ def Parse(javaList):
 
 	importStrings = []
 	functionDict = {}
-	programName = ""
+	programName, currentFuncName = "", ""
 
 	#print(*javaList, sep = '\n')
 	#return	
@@ -137,9 +194,13 @@ def Parse(javaList):
 				strCount -= 1
 			
 			# found a function definition
-			else: 
+			else:
 				# create function dictionary
-				functionDict.update(ExtractFunction(javaList))
+				newFuncDict = ExtractFunction(javaList)
+				functionDict.update(newFuncDict)
+				
+				# get current function name
+				currentFuncName = list(newFuncDict.keys())[0]
 				
 				# pop current element until the open brack is found, 
 				# the definition is not needed anymore
@@ -147,6 +208,18 @@ def Parse(javaList):
 					javaList.pop(0)
 					strCount -= 1
 
+		# found first '{' char of a function
+		if (javaList[0] == "{") and currentFuncName:
+			javaList.pop(0)
+			strCount -= 1
+
+			print("Contents of function \"{}\":".format(currentFuncName))
+			functionDict[currentFuncName]["content"] = ExtractContents(javaList)
+
+			# pop current element until the end of function
+			while javaList[0] != "}":
+				javaList.pop(0)
+				strCount -= 1
 
 		# DEBUG PRINT
 		print(javaList[0], end = " ")
@@ -159,7 +232,7 @@ def Parse(javaList):
 		strCount -= 1
 
 	print("\nParsing Finished\n")
-
+		
 	print("Import Strings:", importStrings)
 	print("Program Name:", programName)
 	print("Function Dictionaries:")
