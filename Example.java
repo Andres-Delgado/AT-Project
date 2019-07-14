ef1cf606120222008ba9c46450377a3ae5add9f4
Daniel Ragbeer <danragbeer@gmail.com>
2019-07-13 20:24:31 -0400

    Fixed parameter extraction for one variable, and updated Example.java

--- Example.java
+++ Example.java
@@ -10,6 +10,19 @@ public class Example {
 		return (a * b);
 	}
 
+	public static void compare(int x) {
+		if(x < 50)
+		{
+			System.out.println("Your favorite number is less than 50");
+		}
+		else
+		{
+			System.out.println("Your favorite number is greater than 50");
+		}
+
+		return;
+	}
+
 	public static void main(String args[]) {
 
 		Scanner input = new Scanner(System.in);
@@ -18,7 +31,8 @@ public class Example {
 		int num1 = input.nextInt();
 		System.out.print("Enter the second number: ");
 		int num2 = input.nextInt();
-
+		System.out.print("Enter your favorite number: ");
+		int x = input.nextInt();
 		input.close();
 
 		int result1 = Add(num1, num2);
@@ -26,6 +40,8 @@ public class Example {
 
 		System.out.println("Addition: " + result1);
 		System.out.println("Multiply: " + result2);
+
+		compare(x);
 
 		return;
 	}
---
+++ Example.py
@@ -0,0 +1,22 @@
+def Add(x, y):
+	return (x + y)
+
+def Multiply(a, b):
+	return (a * b)
+
+def compare(x):
+	print("Your favorite number is greater than 50")
+	return
+
+if __name__ == "__main__":
+	print("Enter the first number: ", end = "")
+	num1 = int(input())
+	print("Enter the second number: ", end = "")
+	num2 = int(input())
+	print("Enter your favorite number: ", end = "")
+	x = int(input())
+	result1 = Add(num1, num2)
+	result2 = Multiply(num1, num2)
+	print("Addition: " + result1)
+	print("Multiply: " + result2)
+
